7a83e93e410997374dc6a74b7eb3ad703058de44
Mr. Hyde <mr.hyde@2bad.me>
2015-04-14 08:21:50 +0200

    - [misc] circle-ci integration

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+PROJECT_NAME=$1
+PROJECT_VERSION=$2
+
+rm -rf $PROJECT_NAME
+unzip $PROJECT_NAME.zip -d $PROJECT_NAME
+
+DOCKER_IMAGE_NAME=$(echo "$PROJECT_NAME" | tr '[:upper:]' '[:lower:]')
+
+docker build -t "$DOCKER_IMAGE_NAME:$PROJECT_VERSION" $PROJECT_NAME
+
+sudo tee /etc/systemd/system/$PROJECT_NAME.service << EOF
+[Unit]
+Description=Nginx based application
+After=docker.service
+Requires=docker.service
+[Service]
+Restart=always
+TimeoutStartSec=5s
+ExecStartPre=-/usr/bin/docker kill $PROJECT_NAME
+ExecStartPre=-/usr/bin/docker rm $PROJECT_NAME
+ExecStart=/usr/bin/docker run --name $PROJECT_NAME -p 80:80 $DOCKER_IMAGE_NAME:$PROJECT_VERSION
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable $PROJECT_NAME.service
+sudo systemctl restart $PROJECT_NAME.service
