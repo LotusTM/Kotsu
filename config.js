18de369fc79fe4501329578d9e5808c0e16f3afb
Mr. Hyde <mr.hyde@2bad.me>
2015-09-09 01:09:58 +0200

    - [grunt] updated jspm and babel to last versions

--- config.js
+++ config.js
@@ -1,32 +1,33 @@
 System.config({
-  "baseURL": "/",
-  "transpiler": "babel",
-  "babelOptions": {
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
     "optional": [
-      "runtime"
+      "runtime",
+      "optimisation.modules.system"
     ]
   },
-  "paths": {
-    "*": "*.js",
-    "github:*": "temp/jspm_packages/github/*.js",
-    "npm:*": "temp/jspm_packages/npm/*.js"
-  }
-});
+  paths: {
+    "github:*": "temp/jspm_packages/github/*",
+    "npm:*": "temp/jspm_packages/npm/*"
+  },
 
-System.config({
-  "map": {
-    "babel": "npm:babel-core@5.4.7",
-    "babel-runtime": "npm:babel-runtime@5.4.7",
-    "core-js": "npm:core-js@0.9.12",
+  map: {
+    "babel": "npm:babel-core@5.8.23",
+    "babel-runtime": "npm:babel-runtime@5.8.20",
+    "core-js": "npm:core-js@1.1.4",
     "jquery": "github:components/jquery@2.1.4",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:core-js@0.9.12": {
+    "npm:babel-runtime@5.8.20": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@1.1.4": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     }
   }
 });
-
