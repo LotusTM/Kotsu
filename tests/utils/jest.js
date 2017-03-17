440be02879d02e4799e60d2d4fa64bfd1a270b36
ArmorDarks <arkahor@gmail.com>
2017-03-17 22:00:30 +0200

    [tests] disable `jest-jspm` due to https://github.com/LotusTM/Kotsu/issues/199

--- tests/utils/jest.js
+++ tests/utils/jest.js
@@ -1,15 +1,17 @@
 const { run } = require('jest')
 const pkg = require('../../package.json')
-const jestJSPM = require('jest-jspm')
+// const jestJSPM = require('jest-jspm')
 
 const argv = process.argv.slice(2)
 
-const jestConfig = jestJSPM(process.cwd(), {
-  jestConfig: pkg.jest,
-  sjsConfigFile: 'jspm.config.js',
-  displayWarnings: true
-})
+// @todo disabked due to https://github.com/LotusTM/Kotsu/issues/199
+// const jestConfig = jestJSPM(process.cwd(), {
+//   jestConfig: pkg.jest,
+//   sjsConfigFile: 'jspm.config.js',
+//   displayWarnings: true
+// })
 
-argv.push('--config', JSON.stringify(jestConfig))
+// argv.push('--config', JSON.stringify(jestConfig))
+argv.push('--config', pkg.jest)
 
 run(argv)
