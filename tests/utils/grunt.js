e3727bee4d6eb17b14f4efc252f1d0e93ed12977
ArmorDarks <arkahor@gmail.com>
2017-04-04 15:52:36 +0300

    [tests] add `grunt` util

---
+++ tests/utils/grunt.js
@@ -0,0 +1,16 @@
+import gt from 'grunt'
+import gruntfile from '../../gruntfile.coffee'
+
+const grunt = gruntfile.call(gt, gt)
+
+/**
+ * Run Grunt instance with specified tasks as args
+ * @param {string[]} [args] Arrays of arguments to be called with Grunt instance, like task names.
+ *                          When not specified will run `default` task
+ * @return {promise} With resolved status on succesful pass, or reject with `new Error()` on failure
+ */
+const runGrunt = (args) => new Promise((resolve, reject) =>
+  grunt.util.spawn({ cmd: 'grunt', args }, (error) => error ? reject(new Error(error)) : resolve('Done!'))
+)
+
+export { grunt, runGrunt }
