477e94b733e2f23823d391399fa4ab72eab98401
ArmorDarks <git@lavrins.com>
2017-07-31 14:55:07 +0300

    [tests][utls] make Nunjucks tests environment more complete

--- tests/utils/nunjucks.js
+++ tests/utils/nunjucks.js
@@ -1,11 +1,14 @@
-import { Environment } from 'nunjucks'
+import nunjucks from 'nunjucks'
 import cloneDeep from 'lodash/cloneDeep'
 import { grunt } from './grunt'
 import nunjucksExtensions from '../../modules/nunjucks-extensions'
+import i18nTools from '../../modules/i18n-tools'
 
-const env = new Environment()
+const env = nunjucks.configure(grunt.config('path.source.templates'))
+const baseLocale = grunt.config('baseLocale')
 
-nunjucksExtensions(env, grunt.config('baseLocale'))
+nunjucksExtensions(env, baseLocale)
+i18nTools.nunjucksExtensions(env, grunt.config('locales'), baseLocale, baseLocale, true)
 
 export const renderString = (template, context, parse) => {
   const rendered = env.renderString(template, cloneDeep(context))
