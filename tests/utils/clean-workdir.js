4f1517a1028dba88dc888436aabfff28ca9fd08a
ArmorDarks <git@lavrins.com>
2017-10-04 23:56:40 +0300

    [tests] improve `clean-workdir` logging

--- tests/utils/clean-workdir.js
+++ tests/utils/clean-workdir.js
@@ -2,21 +2,21 @@ const { exec } = require('child_process')
 
 exec('git --version', (error) => {
   if (error) {
-    console.error(`[clean-workdir] skipping clean working directory test: no Git has been detected\n${error}`)
+    console.error(`[clean-workdir] No Git has been detected.\n\nSkipping clean working directory test.\n\n${error}`)
     return
   }
 
   exec('git status --porcelain', (error, stdout) => {
     if (error) {
-      console.error(`[clean-workdir] an error occured:\n${error}`)
+      console.error(`[clean-workdir] an error occured:\n\n${error}`)
       return
     }
 
     if (stdout) {
-      console.error(`[clean-workdir] build task should not generate or change files outside build folder,\ninstead modified files have been detected:\n\n${stdout}\nexiting with code 1`)
+      console.error(`[clean-workdir] Modified files have been detected.\n\nBuild task should not generate or change files outside build folder:\n\n${stdout}`)
       process.exit(1)
     }
 
-    console.error(`[clean-workdir] no modified files detected outside of build folder. Live long and prosper`)
+    console.log(`[clean-workdir] no modified files detected outside of build folder. Live long and prosper`)
   })
 })
