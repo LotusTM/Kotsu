66153ceb0afade6ccb2fb85214d9a2e9810a00e6
ArmorDarks <git@lavrins.com>
2017-10-05 17:57:35 +0300

    [tests][utils] few readability improvements to `clean-workdir` script

--- tests/utils/clean-workdir.js
+++ tests/utils/clean-workdir.js
@@ -2,19 +2,23 @@ const { exec } = require('child_process')
 
 exec('git --version', (error) => {
   if (error) {
-    console.error(`[clean-workdir] No Git has been detected.\n\nSkipping clean working directory test.\n\n${error}`)
+    console.error('[clean-workdir] No Git has been detected.\n\nSkipping clean working directory test.\n')
+    console.error(error)
     return
   }
 
   exec('git status --porcelain', (error, stdout) => {
     if (error) {
-      console.error(`[clean-workdir] an error occured:\n\n${error}`)
+      console.error('[clean-workdir] an error occured:\n')
+      console.error(error)
       return
     }
 
     if (stdout) {
-      console.error(`[clean-workdir] Modified files have been detected.\n\nBuild task should not generate or change files outside build folder:\n\n${stdout}`)
-      process.exit(1)
+      console.error('[clean-workdir] Modified files have been detected.\n')
+      console.error('Build task should not generate or change files outside build folder:\n')
+      console.error(stdout)
+      return process.exit(1)
     }
 
     console.log(`[clean-workdir] no modified files detected outside of build folder. Live long and prosper`)
