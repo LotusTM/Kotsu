309ed8d6976e7c01c85254ee32ab480e72068656
ArmorDarks <git@lavrins.com>
2018-03-26 03:25:06 +0300

    [modules] always reset cache when rendering string via Nunjucks test utils

--- modules/nunjucks-test-utils.js
+++ modules/nunjucks-test-utils.js
@@ -3,6 +3,7 @@ const cloneDeep = require('lodash/cloneDeep')
 const { grunt } = require('./grunt')
 const nunjucksExtensions = require('./nunjucks-extensions')
 const i18nTools = require('./i18n-tools')
+const { cache } = require('./cache')
 
 const env = nunjucks.configure(grunt.config('path.source.templates'))
 
@@ -13,13 +14,16 @@ i18nTools.nunjucksExtensions(env)
  * Utility function for testing Nunjucks template
  * Renders template with specific context and usual
  * for Kotsu environment
- * @param  {string}  template  Template to render
- * @param  {object}  [context] Template context
- * @param  {boolean} [parse]   Parse output with `JSON.parse`.
- *                             Useful for object or array dumps
+ * @param  {string}  template          Template to render
+ * @param  {object}  [context]         Template context
+ * @param  {boolean} [parse]           Parse output with `JSON.parse`.
+ *                                     Useful for object or array dumps
+ * @param  {boolean} [resetCache=true] Reset cache
  * @return {string} Rendered template
  */
-const renderString = (template, context, parse) => {
+const renderString = (template, context, parse, resetCache = true) => {
+  if (resetCache) cache.reset()
+
   const rendered = env.renderString(template, cloneDeep(context))
 
   if (parse) {
