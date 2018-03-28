aaa872a31ff88276b1a67fc6718ae08fcf2e2872
ArmorDarks <git@lavrins.com>
2018-03-29 00:48:28 +0300

    [scripts] import all data and destructure it later, otherwise it won't work in HMR mode

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,8 +1,10 @@
 // Uncomment if you need to support advanced ES6 features in IE11 and below
 // import 'babel-polyfill'
 
-import { SITE, ENV } from '@data'
+import data from '@data'
 import './utils/dom-polyfills'
+
+const { SITE, ENV } = data
 
 console.log(`${SITE.name} v${SITE.version} #${ENV.buildNumber}, SHA1 ${ENV.buildSHA1}`)
 
